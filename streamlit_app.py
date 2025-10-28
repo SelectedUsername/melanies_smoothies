42727e92623b7a1a7bc16e7dc3e501683c24323f
EducationDataModeler <kellierouege@gmail.com>
2025-10-28 14:39:00 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
+import pandas as pd
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -14,7 +15,7 @@ cnx = st.connection('snowflake')
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit_name'),col('search_on'))
 st.dataframe(data=my_dataframe, use_container_width=True)
-st.stop
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
